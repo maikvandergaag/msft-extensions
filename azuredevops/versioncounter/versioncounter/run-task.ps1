07043e7b00282f6cd26e3ce58d43b08e4b748ce9
Maik van der Gaag <maik@familie-vandergaag.nl>
2018-11-02 15:31:53 +0100

    changed requirements

--- azuredevops/versioncounter/versioncounter/run-task.ps1
+++ azuredevops/versioncounter/versioncounter/run-task.ps1
@@ -2,9 +2,9 @@ Trace-VstsEnteringInvocation $MyInvocation
 
 $VersionVariable = Get-VstsInput -Name VersionVariable -Require
 $UpdateMinorVersion = Get-VstsInput -Name UpdateMinorVersion -Require
-$MaxValuePatchVersion = Get-VstsInput -Name MaxValuePatchVersion -Require
-$MaxValueMinorVersion = Get-VstsInput -Name MaxValueMinorVersion -Require
-$UpdateMinorVersion = Get-VstsInput -Name UpdateMajorVersion -Require
+$MaxValuePatchVersion = Get-VstsInput -Name MaxValuePatchVersion
+$MaxValueMinorVersion = Get-VstsInput -Name MaxValueMinorVersion
+$UpdateMinorVersion = Get-VstsInput -Name UpdateMajorVersion
 $DevOpsPat = Get-VstsInput -Name DevOpsPat -Require
 
 $devOpsUri = $env:SYSTEM_TEAMFOUNDATIONSERVERURI
@@ -12,16 +12,16 @@ $projectName = $env:SYSTEM_TEAMPROJECT
 $projectId = $env:SYSTEM_TEAMPROJECTID 
 $buildId = $env:BUILD_BUILDID
 
-Write-Output "VersionVariable                : $($VersionVariable)";
-Write-Output "UpdateMinorVersion             : $($UpdateMinorVersion)";
-Write-Output "MaxValuePatchVersion           : $($MaxValuePatchVersion)";
-Write-Output "UpdateMajorVersion             : $($UpdateMajorVersion)";
-Write-Output "MaxValueMinorVersion           : $($MaxValueMinorVersion)";
-Write-Output "DevOpsPAT                  	 : $(if (![System.String]::IsNullOrWhiteSpace($DevOpsPAT)) { '***'; } else { '<not present>'; })"; ;
-Write-Output "DevOps Uri           		     : $($devOpsUri)";
-Write-Output "Project Name          	     : $($projectName)";
-Write-Output "Project Id           			 : $($projectId)";
-Write-Output "BuildId            		     : $($buildId)";
+Write-Output "VersionVariable      : $($VersionVariable)";
+Write-Output "UpdateMinorVersion   : $($UpdateMinorVersion)";
+Write-Output "MaxValuePatchVersion : $($MaxValuePatchVersion)";
+Write-Output "UpdateMajorVersion   : $($UpdateMajorVersion)";
+Write-Output "MaxValueMinorVersion : $($MaxValueMinorVersion)";
+Write-Output "DevOpsPAT            : $(if (![System.String]::IsNullOrWhiteSpace($DevOpsPAT)) { '***'; } else { '<not present>'; })"; ;
+Write-Output "DevOps Uri           : $($devOpsUri)";
+Write-Output "Project Name         : $($projectName)";
+Write-Output "Project Id           : $($projectId)";
+Write-Output "BuildId              : $($buildId)";
 
 $buildUri = "$($devOpsUri)$($projectName)/_apis/build/builds/$($buildId)?api-version=4.1"
 
@@ -43,45 +43,50 @@ if ($buildDef) {
         Write-Host "Value of the Major Version Variable: $($definition.variables.$VersionVariable.Value)"
         $version = $definition.variables.$VersionVariable.Value
 
-        if(!$version){
+        if (!$version) {
             $version = "1.0.0"
         }
 
         $items = $version.split('.')
 
-        Write-Host "Value of the Major Version Variable: $($definition.variables.$MajorVersionVariable.Value)"
-        Write-Host "Value of the Minor Version Variable: $($definition.variables.$MinorVersionVariable.Value)"
-        Write-Host "Value of the Patch Version Variable: $($definition.variables.$PatchVersionVariable.Value)"
-
-		$minorVersion = [convert]::ToInt32($definition.variables.$MinorVersionVariable.Value, 10)
-		$majorVersion = [convert]::ToInt32($definition.variables.$MajorVersionVariable.Value, 10)
-		$patchVersion = [convert]::ToInt32($definition.variables.$PatchVersionVariable.Value, 10)
-		
-		$updatedPatchVersion = $patchVersion + 1
-        $updatedMinorVersion = $minorVersion
-        $updatedMajorVersion = $majorVersion
-
-		if(($MaxValuePatchVersion -ne 0) -and ($updatedPatchVersion -gt $MaxValuePatchVersion) -and $UpdateMinorVersion){
-			$updatedPatchVersion = 0
-			$updatedMinorVersion = $updatedMinorVersion + 1
+        if ($items.Count -gt 3 -or $items.Count -lt 3) {
+            Write-Error "The version number needs to be in the following format '1.0.0'"
         }
+        else {
+            $patchVersionVar = $items[2]
+            $minorVersionVar = $items[1]
+            $majorVersionVar = $items[0]
         
-        if(($MaxValueMinorVersion -ne 0) -and ($updatedMinorVersion -gt $MaxValueMinorVersion) -and $UpdateMajorVersion){
-			$updatedMinorVersion = 0
-			$updatedMajorVersion = $majorVersion + 1
-		}
+            Write-Host "Current version Major: $($majorVersionVar), Minor $($minorVersionVar), Patch $($patchVersionVar)"
+  
+            $minorVersion = [convert]::ToInt32($minorVersionVar, 10)
+            $majorVersion = [convert]::ToInt32($majorVersionVar, 10)
+            $patchVersion = [convert]::ToInt32($patchVersionVar, 10)
+            
+            $updatedPatchVersion = $patchVersion + 1
+            $updatedMinorVersion = $minorVersion
+            $updatedMajorVersion = $majorVersion
 
-        Write-Host "Updating patch version number from: $($patchVersion) to $($updatedPatchVersion)."
-        $definition.variables.$PatchVersionVariable.Value = $updatedPatchVersion.ToString()
-        Write-Host "Updating minor version number from: $($minorVersion) to $($updatedMinorVersion)."
-        $definition.variables.$MinorVersionVariable.Value = $updatedMinorVersion.ToString()
-        Write-Host "Updating major version number from: $($majorVersion) to $($updatedMajorVersion)."
-		$definition.variables.$MajorVersionVariable.Value = $updatedMajorVersion.ToString()
-		
-        $definitionJson = $definition | ConvertTo-Json -Depth 50 -Compress
+            if (($MaxValuePatchVersion -ne 0) -and ($updatedPatchVersion -gt $MaxValuePatchVersion) -and $UpdateMinorVersion) {
+                $updatedPatchVersion = 0
+                $updatedMinorVersion = $updatedMinorVersion + 1
+            }
+            
+            if (($MaxValueMinorVersion -ne 0) -and ($updatedMinorVersion -gt $MaxValueMinorVersion) -and $UpdateMajorVersion) {
+                $updatedMinorVersion = 0
+                $updatedMajorVersion = $majorVersion + 1
+            }
 
-        Write-Verbose "Updating Project Build number with URL: $($defUri)"
-        Invoke-RestMethod -Method Put -Uri $defUri -Headers $devOpsHeader -ContentType "application/json" -Body $definitionJson | Out-Null
+            Write-Host "Updating patch version number from: $($patchVersion) to $($updatedPatchVersion)."
+            Write-Host "Updating minor version number from: $($minorVersion) to $($updatedMinorVersion)."
+            Write-Host "Updating major version number from: $($majorVersion) to $($updatedMajorVersion)."
+            $definition.variables.$VersionVariable.Value = "$($updatedMajorVersion).$($updatedMinorVersion).$($updatedPatchVersion)"
+
+            $definitionJson = $definition | ConvertTo-Json -Depth 50 -Compress
+
+            Write-Verbose "Updating Project Build number with URL: $($defUri)"
+            Invoke-RestMethod -Method Put -Uri $defUri -Headers $devOpsHeader -ContentType "application/json" -Body $definitionJson | Out-Null
+        }        
     }
     else {
         Write-Error "The variables can not be found on the definition: $($MajorVersionVariable), $($MinorVersionVariable), $($PatchVersionVariable)"
