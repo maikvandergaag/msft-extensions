507bb25670f1a1ffdd942023d0aa2e25bc30e3fe
Maik van der Gaag <maik@familie-vandergaag.nl>
2019-01-15 09:03:15 +0100

    updated properties

--- azuredevops/triggerpipeline/triggerpipeline/run.ps1
+++ azuredevops/triggerpipeline/triggerpipeline/run.ps1
@@ -8,31 +8,35 @@ try {
 	$connectedServiceName = Get-VstsInput -Name ConnectedServiceName
 	$serviceEndpoint = Get-VstsEndpoint -Name $connectedServiceName -Require
 
+	$json = ConvertTo-Json @serviceEndpoint
+	Write-Host $json
+
 	#connected service
 	$userName =  $serviceEndpoint.Auth.Parameters.username
 	$pat = $serviceEndpoint.Auth.Parameters.password
-	$OrganizationName = $serviceEndpoint.Data.OrganizationName
+	$releaseUrl = $serviceEndpoint.Data.releaseUrl
+	$organizationUrl = $serviceEndpoint.url
 
 	Write-Host "******************************"
 	Write-Host "** Service Connection: $($connectedServiceName)"
-	Write-Host "** Username: $($userName)"
+	Write-Host "** Release Url: $($releaseUrl)"
+	Write-Host "** Organization Url: $($organizationUrl)"
 	Write-Host "** PAT: $($pat)"
-	Write-Host "** Organization name: $($OrganizationName)"
 	Write-Host "******************************"
 
 	#parameters
-	$AzureDevOpsProjectName = Get-VstsInput -Name AzureDevOpsProjectName -Require
-	$PipelineName = Get-VstsInput -Name PipelineName -Require
-	$Pipeline = Get-VstsInput -Name Pipeline -Require
+	$AzureDevOpsProjectName = Get-VstsInput -Name project -Require
+	$BuildPipelineName = Get-VstsInput -Name buildDefinition -Require
+	$ReleasePipelineName = Get-VstsInput -Name releaseDefinition -Require
 	$Description = Get-VstsInput -Name Description
 	$Branch= Get-VstsInput -Name Branch
 	$BuildNumber = Get-VstsInput -Name BuildNumber
 
     if ($Pipeline -eq "Build") {
-        .\run-build.ps1 -AzureDevOpsAccount $AzureDevOpsAccount -AzureDevOpsProjectName $AzureDevOpsProjectName -Username $Username -DevOpsPAT $pat -PipelineName $PipelineName -Description $Description -Branch $Branch
+        .\run-build.ps1 -AzureDevOpsAccount $AzureDevOpsAccount -AzureDevOpsProjectName $AzureDevOpsProjectName -Username $Username -DevOpsPAT $pat -PipelineName $BuildPipelineName -Description $Description -Branch $Branch
     }
     elseif ($Pipeline -eq "Release") {
-        .\run-release.ps1 -AzureDevOpsAccount $AzureDevOpsAccount -AzureDevOpsProjectName $AzureDevOpsProjectName -Username $Username -DevOpsPAT $pat -PipelineName $PipelineName -Description $Description -BuildNumber $BuildNumber
+        .\run-release.ps1 -AzureDevOpsAccount $AzureDevOpsAccount -AzureDevOpsProjectName $AzureDevOpsProjectName -Username $Username -DevOpsPAT $pat -PipelineName $ReleasePipelineName -Description $Description -BuildNumber $BuildNumber
     }  
 }
 finally {
