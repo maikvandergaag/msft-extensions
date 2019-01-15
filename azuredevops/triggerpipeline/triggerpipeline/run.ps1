95d980d7f29e1de5f34d391b03437723b44bb864
Maik van der Gaag <maik@familie-vandergaag.nl>
2019-01-15 09:29:54 +0100

    updated run file

--- azuredevops/triggerpipeline/triggerpipeline/run.ps1
+++ azuredevops/triggerpipeline/triggerpipeline/run.ps1
@@ -13,24 +13,32 @@ try {
 
 	#connected service
 	$userName =  $serviceEndpoint.Auth.Parameters.username
-	$pat = $serviceEndpoint.Auth.Parameters.password
+	$token = $serviceEndpoint.Auth.Parameters.apitoken
 	$releaseUrl = $serviceEndpoint.Data.releaseUrl
 	$organizationUrl = $serviceEndpoint.url
-
-	Write-Host "******************************"
-	Write-Host "** Service Connection: $($connectedServiceName)"
-	Write-Host "** Release Url: $($releaseUrl)"
-	Write-Host "** Organization Url: $($organizationUrl)"
-	Write-Host "** PAT: $($pat)"
-	Write-Host "******************************"
 
 	#parameters
 	$AzureDevOpsProjectName = Get-VstsInput -Name project -Require
 	$BuildPipelineName = Get-VstsInput -Name buildDefinition -Require
 	$ReleasePipelineName = Get-VstsInput -Name releaseDefinition -Require
+	$Pipeline = Get-VstsInput -Name Pipeline -Require
 	$Description = Get-VstsInput -Name Description
 	$Branch= Get-VstsInput -Name Branch
 	$BuildNumber = Get-VstsInput -Name BuildNumber
+
+	Write-Host "******************************"
+	Write-Host "** Service Connection: $($connectedServiceName)"
+	Write-Host "** Release Url: $($releaseUrl)"
+	Write-Host "** Organization Url: $($organizationUrl)"
+	Write-Host "** Token: $($token)"
+	Write-Host "** Project name: $($AzureDevOpsProjectName)"
+	Write-Host "** Pipeline: $($Pipeline)"
+	Write-Host "** Build pipeline: $($BuildPipelineName)"
+	Write-Host "** Release pipeline: $($ReleasePipelineName)"
+	Write-Host "** Description: $($Description)"
+	Write-Host "** Branch: $($Branch)"
+	Write-Host "** BuildNumber: $($BuildNumber)"
+	Write-Host "******************************"
 
     if ($Pipeline -eq "Build") {
         .\run-build.ps1 -AzureDevOpsAccount $AzureDevOpsAccount -AzureDevOpsProjectName $AzureDevOpsProjectName -Username $Username -DevOpsPAT $pat -PipelineName $BuildPipelineName -Description $Description -Branch $Branch
