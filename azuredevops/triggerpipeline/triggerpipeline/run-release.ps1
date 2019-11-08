4939f768d0ecf968eea71606da97b128dc1f286b
maikvandergaag <maik@familie-vandergaag.nl>
2019-11-08 10:16:51 +0100

    updated extension

--- azuredevops/triggerpipeline/triggerpipeline/run-release.ps1
+++ azuredevops/triggerpipeline/triggerpipeline/run-release.ps1
@@ -25,17 +25,17 @@ $RunReleaseUri = "$($baseReleaseUri)$($runRelease)"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("token:{0}" -f $DevOpsPAT)))
 $DevOpsHeaders = @{Authorization = ("Basic {0}" -f $base64AuthInfo)};
 
-$ReleaseDefinitions = Invoke-RestMethod -UseBasicParsing -Uri $ReleaseUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
+$ReleaseDefinitions = Invoke-RestMethod -Uri $ReleaseUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
     
 if ($ReleaseDefinitions -and $ReleaseDefinitions.count -eq 1) {
     $specificUri = $ReleaseDefinitions.value[0].url
-    $Definition = Invoke-RestMethod -UseBasicParsing -Uri $specificUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
+    $Definition = Invoke-RestMethod -Uri $specificUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
 
     if ($BuildNumber) {
         foreach ($artifact in $Definition.artifacts) {
             if ($artifact.isPrimary) {
                 $buildUri = "$($baseBuildUri)_apis/build/builds?definitions=$($artifact.definitionReference.definition.id)&resultfilter=succeeded&buildNumber=$($BuildNumber)&statusFilter=completed&api-version=5.0-preview.5"
-                $buildResult = Invoke-RestMethod -UseBasicParsing -Uri $buildUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
+                $buildResult = Invoke-RestMethod -Uri $buildUri -Method Get -ContentType "application/json" -Headers $DevOpsHeaders;
             
                 if ($buildResult -and $buildResult.count -eq 1) {
                     $artifactsItem = @()
@@ -65,7 +65,7 @@ if ($ReleaseDefinitions -and $ReleaseDefinitions.count -eq 1) {
         $jsonbody = $Release | ConvertTo-Json -Depth 100
 
         try {
-            $Result = Invoke-RestMethod -UseBasicParsing -Uri $RunReleaseUri -Method Post -ContentType "application/json" -Headers $DevOpsHeaders -Body $jsonbody;
+            $Result = Invoke-RestMethod -Uri $RunReleaseUri -Method Post -ContentType "application/json" -Headers $DevOpsHeaders -Body $jsonbody;
         } catch {
             if($_.ErrorDetails.Message){
 
