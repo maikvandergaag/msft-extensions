470e5d3be3166daf145d0e71119d1ec3db2c0827
Maik van der Gaag <maik@familie-vandergaag.nl>
2020-01-06 16:28:48 +0100

    added scope

--- azuredevops/arm-ttk/arm-ttk/run-task.ps1
+++ azuredevops/arm-ttk/arm-ttk/run-task.ps1
@@ -4,7 +4,7 @@ $templatefolder = Get-VstsInput -Name TemplateFolder -Require
 
 Write-Host "Template Folder:      $templatefolder"
 
-Install-Module Pester
+Install-Module Pester -Force -Scope CurrentUser
 
 Import-Module "$PSScriptRoot\ps_modules\arm-ttk\arm-ttk.psd1"
 
