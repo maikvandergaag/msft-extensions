03b57c5a0a967eb3cb73b3fff218e81ed9c85bcb
Maik van der Gaag <maik@familie-vandergaag.nl>
2020-01-06 15:48:34 +0100

    added continue action

--- azuredevops/arm-ttk/arm-ttk/run-task.ps1
+++ azuredevops/arm-ttk/arm-ttk/run-task.ps1
@@ -6,6 +6,6 @@ Write-Host "Template Folder:      $templatefolder"
 
 Import-Module "$PSScriptRoot\ps_modules\arm-ttk\arm-ttk.psd1"
 
-$output = Test-AzTemplate "$templatefolder"
+Test-AzTemplate "$templatefolder" -ErrorAction SilentlyContinue
 
 Write-Host $output
