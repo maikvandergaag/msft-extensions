54b1fd5466db0f0954f1b11de9b886dfe3d53d74
Maik van der Gaag <maik@familie-vandergaag.nl>
2020-01-08 16:31:39 +0100

    updated script

--- azuredevops/arm-ttk/arm-ttk/run-task.ps1
+++ azuredevops/arm-ttk/arm-ttk/run-task.ps1
@@ -2,12 +2,11 @@ Trace-VstsEnteringInvocation $MyInvocation
 
 $templatefolder = Get-VstsInput -Name TemplateFolder -Require
 
-$ErrorActionPreference = 'SilentlyContinue'
-
-Write-Host "Template Folder:      $templatefolder"
+Write-Host "Importing ARM TTK Module"
 Import-Module "$PSScriptRoot\ps_modules\arm-ttk" -Verbose
 
-$testOutput = @(Test-AzTemplate -TemplatePath \"$templatefolder\")
+Write-Host "## Template Folder:      $templatefolder"
+$testOutput = @(Test-AzTemplate $templatefolder )
 
 $testOutput 
 if ($testOutput | ? {$_.Errors }) { exit 1 } else {   exit 0\n}
