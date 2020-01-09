677bce78f3cd6702de4de89bcff09ef164aed7fe
Maik van der Gaag <maik@familie-vandergaag.nl>
2020-01-09 11:16:36 +0100

    added script information

--- azuredevops/arm-ttk/arm-ttk/run-task.ps1
+++ azuredevops/arm-ttk/arm-ttk/run-task.ps1
@@ -1,9 +1,19 @@
+Write-Host "** Starting ARM Template test"
+
+
 Trace-VstsEnteringInvocation $MyInvocation
 $templatefolder = Get-VstsInput -Name TemplateFolder -Require
 
 $ErrorActionPreference = "Continue"
 
-Import-Module $PSScriptRoot\ps_modules\arm-ttk\arm-ttk.psd1 -Verbose
+$moduleFolder = "$PSScriptRoot\ps_modules\arm-ttk\arm-ttk.psd1"
+
+Write-Host "Importing module from the directory: $moduleFolder"
+
+Import-Module "$moduleFolder" -Verbose
+
+Write-Host = "Testing the the ARM templates in the folder"
+
 $testOutput = @(Test-AzTemplate -TemplatePath $templatefolder)
 
 $testOutput 
